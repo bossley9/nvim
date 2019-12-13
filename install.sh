833f9b707081ce4e32422888609a34ccb760b67b
sam bossley <35381535+bossley9@users.noreply.github.com>
2019-12-13 14:43:55 -0500

    terminal color output to differentiate

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+BLE='\033[1;34m'
+GRN='\033[1;32m'
+NC='\033[0m'
+
 # 1. get latest version of the nvim configuration
 
-echo "updating nvim configuration..."
+echo -e "${BLE}updating nvim configuration...${NC}"
 
 curl https://raw.githubusercontent.com/bossley9/nvim-config/master/init.vim -o /tmp/init.vim
 
 # 2. install nvim and vim-plug if not already installed
 
 if ! hash nvim 2>/dev/null; then
-  echo "installing neovim..."
+  echo -e "${BLE}installing neovim...${NC}"
   sudo add-apt-repository ppa:neovim-ppa/stable
   sudo apt-get update
   sudo apt-get install neovim
 fi
 
 
-echo "installing vim-plug..."
+echo -e "${BLE}installing vim-plug...${NC}"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -33,3 +37,6 @@ mv /tmp/init.vim ~/.config/nvim/
 # install vim-plug plugins
 
 nvim +PlugInstall +qa
+
+
+echo -e "${GRN}done.${BLE} You can now type 'nvim' to start nvim.${NC}"
